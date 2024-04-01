e5f609c81e191069a34249111550e534c0d90650
N100-OppSpark <picks5@naver.com>
2024-04-01 02:17:36 +0000

    15624번 boj 완료

---
+++ BOJ_algo_python/15624.py
@@ -0,0 +1,12 @@
+#15624번: 피보나치 수 7
+def fibo(n):
+    dp = [0]*(n+1)
+    dp[0],dp[1] = 0, 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] % 1000000007
+    return dp[n]
+
+fibos = int(input())
+
+print(fibo(fibos))
