cd0c1ae423245bb2e7b97e55a609858e592aa5af
OppSpark <picks5@naver.com>
2024-07-02 16:48:11 +0900

    1157

---
+++ 2024_1_skillflo_alogs/Ch1_String/1919/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.next();
+    String b = sc.next();
+
+    int[] cntA = new int[26];
+    int[] cntB = new int[26];
+
+    for(int i = 0; i < a.length(); i++)
+        cntA[a.charAt(i) - 'a']++;
+    
+    for(int i = 0; i < b.length(); i++)
+        cntB[b.charAt(i) - 'a']++;
+
+        int answer = 0;
+
+        for(int i = 0; i < 26; i ++){
+            if(cntA[i] > cntB[i])
+                answer += cntA[i] - cntB[i];
+            else
+                answer += cntB[i] - cntA[i];
+        }
+        System.out.println(answer);
+    }
+}
